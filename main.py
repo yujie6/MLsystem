0ca1082ef9a57dae58516d6ee4d758dc1a466df5
yujie6 <yujie6@sjtu.edu.cn>
2019-07-12 16:40:12 +0800

    learn tf

--- main.py
+++ main.py
@@ -5,23 +5,5 @@ import autodiff as ad
 from vector import *
 
 
-def test_mul_two_vars():
-    x2 = ad.Variable(name="x2")
-    x3 = ad.Variable(name="x3")
-    y = x2 * x3
-
-    grad_x2, grad_x3 = ad.gradients(y, [x2, x3])
-
-    executor = ad.Executor([y, grad_x2, grad_x3])
-    x2_val = 2 * np.ones(3)
-    x3_val = 3 * np.ones(3)
-    y_val, grad_x2_val, grad_x3_val = executor.run(feed_dict={x2: x2_val, x3: x3_val})
-
-    assert isinstance(y, ad.Node)
-    assert np.array_equal(y_val, x2_val * x3_val)
-    assert np.array_equal(grad_x2_val, x3_val)
-    assert np.array_equal(grad_x3_val, x2_val)
-
-
 if __name__ == "__main__":
-    test_mul_two_vars()
+    print("hello")
